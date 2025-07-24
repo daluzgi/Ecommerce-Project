bd23c2ae4a6bc4d7bc1a6106dbe6b4dc9cafa7bc
Georgina Da Luz <daluz.georgina@gmail.com>
2025-07-24 14:51:54 -0300

    Navegacion del carrito agregada.

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,34 @@
 import { FiShoppingCart } from "react-icons/fi";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
 
 function CartWidget() {
+  const { getTotalQuantity } = useContext(CartContext);
+  const totalItems = getTotalQuantity();
+
   return (
-    <div>
-      <FiShoppingCart />
-    </div>
+    <Link to="/cart" style={{ position: "relative", display: "inline-block" }}>
+      <FiShoppingCart size={24} />
+      {totalItems > 0 && (
+        <span
+          style={{
+            position: "absolute",
+            top: -8,
+            right: -10,
+            backgroundColor: "red",
+            color: "white",
+            borderRadius: "50%",
+            padding: "2px 6px",
+            fontSize: "0.8rem",
+            fontWeight: "bold",
+          }}
+        >
+          {totalItems}
+        </span>
+      )}
+    </Link>
   );
 }
+
 export default CartWidget;
